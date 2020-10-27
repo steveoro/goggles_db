a86e50303bb6f11ecfb41810a1614966d41a7ab6
steveoro <steve.alloro@gmail.com>
2020-10-27 09:46:52 +0100

    minor tweaks to the Guardfile

--- lib/goggles_db/version.rb
+++ lib/goggles_db/version.rb
@@ -22,7 +22,7 @@ module GogglesDb
     MINOR   = '015'
 
     # Current build version.
-    BUILD   = '20201026'
+    BUILD   = '20201027'
 
     # Full versioning for the current release (Framework + Core).
     FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE} v. #{VERSION})"
