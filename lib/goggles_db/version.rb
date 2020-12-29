cc2dcb3c334a2beafae18631bc82874410064269
steveoro <steve.alloro@gmail.com>
2020-12-29 12:29:49 +0100

    improved SwimmingPool factory to avoid random validation failures due to unique index constraint violation

--- lib/goggles_db/version.rb
+++ lib/goggles_db/version.rb
@@ -9,7 +9,7 @@
 #   Semantic Versioning implementation.
 module GogglesDb
   # Gem version
-  VERSION = '0.1.54'
+  VERSION = '0.1.55'
 
   module Version
     # Framework Core internal name.
@@ -19,10 +19,10 @@ module GogglesDb
     MAJOR   = '7'
 
     # Minor version.
-    MINOR   = '054'
+    MINOR   = '055'
 
     # Current build version.
-    BUILD   = '20201228'
+    BUILD   = '20201229'
 
     # Full versioning for the current release (Framework + Core).
     FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE} v. #{VERSION})"
