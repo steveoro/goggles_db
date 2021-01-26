0a1bf8e2471d359bbfb6a2bab6acc4fa0187652d
steveoro <steve.alloro@gmail.com>
2021-01-26 14:05:30 +0100

    config: added API acronym to default inflections & refactored ApiDailyUse to APIDailyUse

--- spec/factories/goggles_db/api_daily_uses.rb
+++ spec/factories/goggles_db/api_daily_uses.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :api_daily_use, class: 'GogglesDb::ApiDailyUse' do
+  factory :api_daily_use, class: 'GogglesDb::APIDailyUse' do
     sequence(:route) { |n| "#{%w[GET POST PUT DELETE].sample} api/v3/#{FFaker::Lorem.word}/#{FFaker::Lorem.word}/#{n}" }
     day   { Date.today }
     count { 0 }
--- spec/models/goggles_db/api_daily_use_spec.rb
+++ spec/models/goggles_db/api_daily_use_spec.rb
@@ -6,10 +6,10 @@ require 'support/shared_sorting_scopes_examples'
 require 'support/shared_filtering_scopes_examples'
 
 module GogglesDb
-  RSpec.describe ApiDailyUse, type: :model do
-    shared_examples_for 'a valid ApiDailyUse instance' do
+  RSpec.describe APIDailyUse, type: :model do
+    shared_examples_for 'a valid APIDailyUse instance' do
       it 'is valid' do
-        expect(subject).to be_an(ApiDailyUse).and be_valid
+        expect(subject).to be_an(APIDailyUse).and be_valid
       end
 
       # Presence of fields:
@@ -26,17 +26,17 @@ module GogglesDb
 
     context 'when using the factory, the resulting instance' do
       subject { FactoryBot.create(:api_daily_use) }
-      it_behaves_like('a valid ApiDailyUse instance')
+      it_behaves_like('a valid APIDailyUse instance')
     end
     #-- ------------------------------------------------------------------------
     #++
 
     let(:minimum_domain) do
       ((Date.today - 7.days)..Date.today).each { |day| FactoryBot.create(:api_daily_use, day: day) }
-      expect(ApiDailyUse.count).to be >= 7
+      expect(APIDailyUse.count).to be >= 7
       # Add 1 more row just to have a static route to test:
       FactoryBot.create(:api_daily_use, route: 'GET /fake/route')
-      ApiDailyUse.all
+      APIDailyUse.all
     end
 
     before(:each) { expect(minimum_domain.count).to be_positive }
@@ -44,15 +44,15 @@ module GogglesDb
     # Sorting scopes:
     describe 'self.by_date' do
       let(:result) { minimum_domain.by_date }
-      it_behaves_like('sorting scope by_<ANY_VALUE_NAME> (with prepared result)', ApiDailyUse, 'day')
+      it_behaves_like('sorting scope by_<ANY_VALUE_NAME> (with prepared result)', APIDailyUse, 'day')
     end
 
     # Filtering scopes:
     describe 'self.for_date' do
-      it_behaves_like('filtering scope for_<ANY_CHOSEN_FILTER>', ApiDailyUse, 'for_date', 'day', Date.today)
+      it_behaves_like('filtering scope for_<ANY_CHOSEN_FILTER>', APIDailyUse, 'for_date', 'day', Date.today)
     end
     describe 'self.for_route' do
-      it_behaves_like('filtering scope for_<ANY_CHOSEN_FILTER>', ApiDailyUse, 'for_route', 'route', 'GET /fake/route')
+      it_behaves_like('filtering scope for_<ANY_CHOSEN_FILTER>', APIDailyUse, 'for_route', 'route', 'GET /fake/route')
     end
     #-- ------------------------------------------------------------------------
     #++
@@ -62,7 +62,7 @@ module GogglesDb
         let(:existing_row) { minimum_domain.sample }
 
         it 'increases its value by 1' do
-          expect { ApiDailyUse.increase_for!(existing_row.route, existing_row.day) }.to change { existing_row.reload.count }.by(1)
+          expect { APIDailyUse.increase_for!(existing_row.route, existing_row.day) }.to change { existing_row.reload.count }.by(1)
         end
       end
 
@@ -70,8 +70,8 @@ module GogglesDb
         let(:new_built_row) { FactoryBot.build(:api_daily_use) }
 
         it 'creates a corresponding row, setting its initial value to 1' do
-          expect { ApiDailyUse.increase_for!(new_built_row.route, new_built_row.day) }.to change { ApiDailyUse.count }.by(1)
-          expect(ApiDailyUse.where(route: new_built_row.route, day: new_built_row.day).first.count).to eq(1)
+          expect { APIDailyUse.increase_for!(new_built_row.route, new_built_row.day) }.to change { APIDailyUse.count }.by(1)
+          expect(APIDailyUse.where(route: new_built_row.route, day: new_built_row.day).first.count).to eq(1)
         end
       end
     end
