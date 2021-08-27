543598ea785ca2f08562744002c239a7bd5dca95
steveoro <steve.alloro@gmail.com>
2021-08-27 17:54:25 +0200

    moved ImportQueueDecorator here; v. 0.3.27

---
+++ app/decorators/goggles_db/import_queue_decorator.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+module GogglesDb
+  # = ImportQueueDecorator
+  #
+  class ImportQueueDecorator < Draper::Decorator
+    delegate_all
+
+    # Add explicit delegation for methods needed by Kaminari (if the object is an AR::Relation):
+    delegate :current_page, :total_pages, :limit_value, :total_count, :offset_value, :last_page?
+
+    # Returns the a text label for the processing state of this row.
+    def state_flag
+      return '🟢' if done? # (about to be deleted)
+      return '' if process_runs.zero? # (not-yet processed => no status)
+      return "▶ #{process_runs}" if process_runs.positive? # (show number of runs)
+    end
+
+    # Returns the a bespoke text label describing this row, depending on the
+    # group UID.
+    def text_label
+      case uid
+      when 'chrono'
+        "⏱ #{req_event_type&.label}: #{req_timing}, #{req_swimmer_name} (#{req_swimmer_year_of_birth}) #{state_flag}"
+      when /chrono-\d+/
+        "#{req_timing}, #{req_length_in_meters} m #{state_flag}"
+      when 'res'
+        "📌 #{req_event_type&.label}: #{req_timing}, #{req_swimmer_name} by #{user.name} #{state_flag}"
+      else
+        "#{target_entity} by #{user.name} #{state_flag}"
+      end
+    end
+
+    # Returns the associated Swimmer <tt>year_of_birth</tt> found by searching at a maximum
+    # depth of 1, starting at the root-key depth (0) of the request, or +nil+ when
+    # not found.
+    #
+    # === Supported layouts by priority:
+    #
+    # 1. /swimmer => <field_name>
+    # 2. /<anything> => /swimmer => <field_name> (but not deeper)
+    #
+    def req_swimmer_year_of_birth
+      return req&.fetch(root_key, nil)&.fetch('year_of_birth', nil) if root_key == 'swimmer'
+
+      req&.fetch(root_key, nil)&.fetch('swimmer', nil)&.fetch('year_of_birth', nil)
+    end
+  end
+end
---
+++ spec/decorators/goggles_db/import_queue_decorator_spec.rb
@@ -0,0 +1,195 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+module GogglesDb
+  RSpec.describe ImportQueueDecorator do
+    describe '#state_flag' do
+      context 'for a row which is done (but not yet deleted),' do
+        let(:model_obj) { FactoryBot.create(:import_queue, done: true) }
+        subject { described_class.decorate(model_obj) }
+        it 'returns a green dot' do
+          expect(subject.state_flag).to eq('🟢')
+        end
+      end
+
+      context "for a row which hasn\'t been processed yet," do
+        let(:model_obj) { FactoryBot.create(:import_queue) }
+        subject { described_class.decorate(model_obj) }
+        it 'returns an empty string' do
+          expect(subject.state_flag).to be_a(String).and be_empty
+        end
+      end
+
+      context 'for a row that has already been processed at least once,' do
+        let(:model_obj) { FactoryBot.create(:import_queue, process_runs: 1) }
+        subject { described_class.decorate(model_obj) }
+        it 'returns the counter of runs' do
+          expect(subject.state_flag).to eq('▶ 1')
+        end
+      end
+    end
+    #-- -------------------------------------------------------------------------
+    #++
+
+    let(:fixture_swimmer) { FactoryBot.build(:swimmer) }
+    let(:fixture_event_type) { GogglesDb::EventType.all.sample }
+
+    describe '#text_label' do
+      let(:minutes) { (rand * 5).to_i }
+      let(:seconds) { (rand * 59).to_i }
+      let(:hundredths) { (rand * 99).to_i }
+      let(:meters) { 50 + (rand * 8).to_i * 50 }
+      let(:timing_request_data) do
+        {
+          'target_entity' => 'Lap',
+          'lap' => {
+            'swimmer' => { 'complete_name' => fixture_swimmer.complete_name },
+            'meeting_individual_result' => {
+              'event_type_id' => fixture_event_type.id
+            },
+            'length_in_meters' => meters,
+            'minutes' => minutes,
+            'seconds' => seconds,
+            'hundredths' => hundredths
+          }
+        }.to_json
+      end
+      before(:each) do
+        expect(fixture_swimmer).to be_a(GogglesDb::Swimmer)
+        expect(fixture_event_type).to be_a(GogglesDb::EventType).and be_valid
+        expect(timing_request_data).to be_a(String).and be_present
+      end
+
+      context 'for a row that contains a valid parent chrono timing,' do
+        let(:fixture_row) { FactoryBot.create(:import_queue, uid: 'chrono', request_data: timing_request_data) }
+        subject { described_class.decorate(fixture_row) }
+
+        it 'includes a chrono icon' do
+          expect(subject.text_label).to include('⏱')
+        end
+        it 'includes the timing from the request data' do
+          expect(subject.text_label).to include(fixture_row.req_timing.to_s)
+        end
+        it 'includes the event type from the request data' do
+          expect(subject.text_label).to include(fixture_row.req_event_type&.label)
+        end
+        it 'includes the swimmer name from the request data' do
+          expect(subject.text_label).to include(fixture_row.req_swimmer_name)
+        end
+      end
+
+      context 'for a row that contains a valid sibling chrono timing,' do
+        let(:fixture_row) { FactoryBot.create(:import_queue, uid: 'chrono-1', request_data: timing_request_data) }
+        subject { described_class.decorate(fixture_row) }
+
+        it 'includes the timing from the request data' do
+          expect(subject.text_label).to include(fixture_row.req_timing.to_s)
+        end
+        it 'includes the length in meters from the request data' do
+          expect(subject.text_label).to include(meters.to_s)
+        end
+        it 'does not include the swimmer name' do
+          expect(subject.text_label).not_to include(fixture_row.req_swimmer_name)
+        end
+      end
+
+      context 'for a row that contains valid meeting reservation data,' do
+        let(:fixture_row) { FactoryBot.create(:import_queue, uid: 'res', request_data: timing_request_data) }
+        subject { described_class.decorate(fixture_row) }
+
+        it 'includes a pin icon' do
+          expect(subject.text_label).to include('📌')
+        end
+        it 'includes the timing from the request data' do
+          expect(subject.text_label).to include(fixture_row.req_timing.to_s)
+        end
+        it 'includes the event type from the request data' do
+          expect(subject.text_label).to include(fixture_row.req_event_type&.label)
+        end
+        it 'includes the swimmer name from the request data' do
+          expect(subject.text_label).to include(fixture_row.req_swimmer_name)
+        end
+        it 'includes the user name that created the request' do
+          expect(subject.text_label).to include(fixture_row.user.name)
+        end
+      end
+
+      context 'for a generic row that contains any other entity data,' do
+        let(:fixture_row) { FactoryBot.create(:import_queue, request_data: timing_request_data) }
+        subject { described_class.decorate(fixture_row) }
+
+        it 'includes the target entity name of the request' do
+          expect(subject.text_label).to include(fixture_row.target_entity)
+        end
+        it 'includes the user name that created the request' do
+          expect(subject.text_label).to include(fixture_row.user.name)
+        end
+      end
+    end
+    #-- -------------------------------------------------------------------------
+    #++
+
+    describe '#req_swimmer_year_of_birth' do
+      before(:each) do
+        expect(fixture_swimmer).to be_a(GogglesDb::Swimmer)
+        expect(fixture_event_type).to be_a(GogglesDb::EventType).and be_valid
+      end
+
+      context 'for a row that contains birth year data nested under a Swimmer node at depth 1,' do
+        let(:fixture_request_data) do
+          {
+            'target_entity' => 'Whatever',
+            'whatever' => {
+              'swimmer' => {
+                'complete_name' => fixture_swimmer.complete_name,
+                'year_of_birth' => fixture_swimmer.year_of_birth
+              }
+            }
+          }.to_json
+        end
+        let(:fixture_row) { FactoryBot.create(:import_queue, request_data: fixture_request_data) }
+        subject { described_class.decorate(fixture_row) }
+
+        it 'returns the year of birth of the swimmer' do
+          expect(subject.req_swimmer_year_of_birth).to eq(fixture_swimmer.year_of_birth)
+        end
+      end
+
+      context 'for a row that contains birth year data nested at root level,' do
+        let(:fixture_request_data) do
+          {
+            'target_entity' => 'Swimmer',
+            'swimmer' => {
+              'complete_name' => fixture_swimmer.complete_name,
+              'year_of_birth' => fixture_swimmer.year_of_birth
+            }
+          }.to_json
+        end
+        let(:fixture_row) { FactoryBot.create(:import_queue, request_data: fixture_request_data) }
+        subject { described_class.decorate(fixture_row) }
+
+        it 'returns the year of birth of the swimmer' do
+          expect(subject.req_swimmer_year_of_birth).to eq(fixture_swimmer.year_of_birth)
+        end
+      end
+
+      context "for a row that doesn't contain any birth year data," do
+        let(:fixture_request_data) do
+          {
+            'target_entity' => 'Swimmer',
+            'swimmer' => { 'id' => (rand * 150).to_i } # (Don't care if it's existing or not)
+          }.to_json
+        end
+        let(:fixture_row) { FactoryBot.create(:import_queue, request_data: fixture_request_data) }
+        subject { described_class.decorate(fixture_row) }
+
+        it 'is nil' do
+          expect(subject.req_swimmer_year_of_birth).to be nil
+        end
+      end
+    end
+    #-- -------------------------------------------------------------------------
+    #++
+  end
+end
