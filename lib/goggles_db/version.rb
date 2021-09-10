29c45fc28daea60815e5b2c04f361615f1a564f7
steveoro <steve.alloro@gmail.com>
2021-09-10 16:55:20 +0200

    upgrade to Rails 6.0.4.1 due to security fixes

--- lib/goggles_db/version.rb
+++ lib/goggles_db/version.rb
@@ -3,12 +3,12 @@
 #
 # = Version module
 #
-#   - version:  7-0.3.27
+#   - version:  7-0.3.29
 #   - author:   Steve A.
 #
 module GogglesDb
   # Public gem version (uses Semantic versioning)
-  VERSION = '0.3.27'
+  VERSION = '0.3.29'
 
   # == Versioning codes
   #
@@ -21,8 +21,8 @@ module GogglesDb
     CORE  = 'C7'
     MAJOR = '0'
     MINOR = '3'
-    PATCH = '27'
-    BUILD = '20210827'
+    PATCH = '29'
+    BUILD = '20210910'
 
     # Full label
     FULL = "#{MAJOR}.#{MINOR}.#{PATCH} (#{CORE}-#{BUILD})"
