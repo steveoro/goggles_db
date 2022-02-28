1d04395d3fc38490989ae6d811a712f55f66e4e1
steveoro <steve.alloro@gmail.com>
2022-02-28 17:38:43 +0100

    additional data-fix for badges with empty numbers

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_141224) do
+ActiveRecord::Schema.define(version: 2022_02_28_161427) do
 
   create_table "achievement_rows", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "lock_version", default: 0
