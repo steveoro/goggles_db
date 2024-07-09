2a9042c881ef83628bee44cf8564b999916f647b
steveoro <steve.alloro@gmail.com>
2024-07-09 11:04:28 +0200

    fixed MariaDB required version for CircleCI build & minor bundle update

--- lib/goggles_db/version.rb
+++ lib/goggles_db/version.rb
@@ -6,7 +6,7 @@
 #
 module GogglesDb
   # Public gem version (uses Semantic versioning)
-  VERSION = '0.7.11'
+  VERSION = '0.7.12'
 
   # == Versioning codes
   #
@@ -19,8 +19,8 @@ module GogglesDb
     CORE  = 'C7'
     MAJOR = '0'
     MINOR = '7'
-    PATCH = '11'
-    BUILD = '20240708'
+    PATCH = '12'
+    BUILD = '20240709'
 
     # Full label
     FULL = "#{MAJOR}.#{MINOR}.#{PATCH} (#{CORE}-#{BUILD})".freeze
