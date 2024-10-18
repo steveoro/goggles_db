aafc594c0d281b4c8fb66d722ba894709614f655
steveoro <steve.alloro@gmail.com>
2024-10-18 19:00:57 +0200

    added a missing/new EventType used for Coopernuoto meetings ('6X50SL') + bundle updates

--- lib/goggles_db/version.rb
+++ lib/goggles_db/version.rb
@@ -6,7 +6,7 @@
 #
 module GogglesDb
   # Public gem version (uses Semantic versioning)
-  VERSION = '0.7.19'
+  VERSION = '0.7.20'
 
   # == Versioning codes
   #
@@ -19,8 +19,8 @@ module GogglesDb
     CORE  = 'C7'
     MAJOR = '0'
     MINOR = '7'
-    PATCH = '19'
-    BUILD = '20240918'
+    PATCH = '20'
+    BUILD = '20241018'
 
     # Full label
     FULL = "#{MAJOR}.#{MINOR}.#{PATCH} (#{CORE}-#{BUILD})".freeze
