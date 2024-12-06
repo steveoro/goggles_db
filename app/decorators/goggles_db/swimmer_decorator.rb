3f4166ddc723ce15d0678a1390c25b048dd7d55f
steveoro <steve.alloro@gmail.com>
2024-12-06 19:13:10 +0100

    added safe navigation for SwimmerDecorator#display_label, when gender is not defined

--- app/decorators/goggles_db/swimmer_decorator.rb
+++ app/decorators/goggles_db/swimmer_decorator.rb
@@ -13,7 +13,7 @@ module GogglesDb
     # == Params
     # - <tt>locale_code</tt>: the locale code override; defaults to <tt>I18n.locale</tt>
     def display_label(locale_code = I18n.locale)
-      "#{complete_name} (#{gender_type.label(locale_code)}, #{year_of_birth}#{year_guessed ? '~' : ''})"
+      "#{complete_name} (#{gender_type&.label(locale_code)}, #{year_of_birth}#{year_guessed ? '~' : ''})"
     end
 
     # Alternative label method for displaying a shorter version of the data available
