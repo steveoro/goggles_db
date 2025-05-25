9198e650c780464561dc93b72aeb1226f077c0a9
steveoro <steve.alloro@gmail.com>
2025-05-26 00:06:59 +0200

    removed wrong view renaming migration and fixed remaining view migrations; DB vers. 2.08.07; vers. 0.8.07

--- db/migrate/20250502210440_create_best_50_and_100_results.rb
+++ db/migrate/20250502210440_create_best_50_and_100_results.rb
@@ -2,11 +2,11 @@
 
 class CreateBest50And100Results < ActiveRecord::Migration[6.1]
   def up
-    create_view :best50_and100_results, version: 1
+    create_view :best_50_and_100_results, version: 1
   end
 
   def down
     # Use raw SQL for robust dropping and existence checking
-    execute 'DROP VIEW IF EXISTS best50_and100_results;'
+    execute 'DROP VIEW IF EXISTS best_50_and_100_results;'
   end
 end
