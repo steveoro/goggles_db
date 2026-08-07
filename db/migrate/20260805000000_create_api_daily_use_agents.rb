f5e8c382da3c97b9b34a915984ddfc42096dc039
steveoro <steve.alloro@gmail.com>
2026-08-07 13:15:33 +0200

    fixed missing require in migration file

--- db/migrate/20260805000000_create_api_daily_use_agents.rb
+++ db/migrate/20260805000000_create_api_daily_use_agents.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'goggles_db/version'
+
 class CreateAPIDailyUseAgents < ActiveRecord::Migration[6.1]
   def up
     create_table :api_daily_use_agents do |t|
