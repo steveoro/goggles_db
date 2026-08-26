1c0ddd5bd23b7f83b410edb962dc38d365863c8c
steveoro <steve.alloro@gmail.com>
2026-08-26 17:15:46 +0200

    minor cosmetic changes on goggle cup shared partials

--- lib/goggles_db/version.rb
+++ lib/goggles_db/version.rb
@@ -6,7 +6,7 @@
 #
 module GogglesDb
   # Public gem version (uses Semantic versioning)
-  VERSION = '0.10.40'
+  VERSION = '0.10.41'
 
   # == Versioning codes
   #
@@ -19,8 +19,8 @@ module GogglesDb
     CORE  = 'C7'
     MAJOR = '0'
     MINOR = '10'
-    PATCH = '40'
-    BUILD = '20260825'
+    PATCH = '41'
+    BUILD = '20260826'
 
     # Full label
     FULL = "#{MAJOR}.#{MINOR}.#{PATCH} (#{CORE}-#{BUILD})".freeze
